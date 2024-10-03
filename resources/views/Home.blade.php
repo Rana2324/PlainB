343d54ee34b3e64d6c262a55d9f8b2ae27776fcb
Rana2324 <shohelranatonmoy98@gmail.com>
2024-10-04 03:52:16 +0900

    create only design by bootstrap in laravel practice project

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,9 @@
+@extends('Layout.app')
+
+@section('content')
+    @include('Component.hero')
+    @include('Component.howWedo')
+    @include('Component.pricing')
+    @include('Component.team')
+    @include('Component.newsLatter')
+@endsection
